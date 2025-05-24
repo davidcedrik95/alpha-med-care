a98dec80eaf127bbcbfb05acbb016f232f71a295
davidcedrik95 <moukam2@gmail.com>
2025-05-24 23:42:46 +0200

    new

--- src/components/nav/MedicalNavigatorMenu.vue
+++ src/components/nav/MedicalNavigatorMenu.vue
@@ -3,7 +3,7 @@
     <v-container class="d-flex align-center">
       <!-- Logo / Titre du site -->
       <div class="d-flex align-center hidden-md-and-up">
-        <v-icon icon="mdi-chair-school" size="large" color="primary" class="mr-2"></v-icon>
+        <v-icon icon="mdi-chair-school" size="large" class="mr-2" style="color: #626363;"></v-icon>
         <v-toolbar-title class="app-title">{{ $t('app.title') }}</v-toolbar-title>
       </div>
 
@@ -239,7 +239,8 @@ const menuCategories = [
 
 .app-title {
   font-weight: bold;
-   color: #626363 !important;
+  color: #626363 !important;
+  font-size: 1rem;
 }
 
 .nav-btn {
