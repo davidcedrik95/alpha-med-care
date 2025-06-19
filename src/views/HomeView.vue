9271a38b7c7fc024237620823094bcbdcc5fd5b2
davidcedrik95 <moukam2@gmail.com>
2025-06-19 23:06:09 +0200

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -350,7 +350,7 @@
             </div>
             <div class="contact-info">
               <h4>Telefonischer Support</h4>
-              <a href="tel:+49123456789" class="contact-link">+49 123 456 789</a>
+              <a href="tel:+499666188950" class="contact-link">+49(0)9666188950</a>
               <p>Mo-Fr: 8:00-18:00 Uhr</p>
             </div>
           </div>
